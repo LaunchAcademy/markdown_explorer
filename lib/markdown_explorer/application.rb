ceec2b35fa57229a7c8cc501697333d7b4bc71a1
Richard Davis <rdavis.bacs@gmail.com>
2016-06-22 11:43:35 -0400

    success!

--- lib/markdown_explorer/application.rb
+++ lib/markdown_explorer/application.rb
@@ -2,7 +2,13 @@ require "sinatra"
 
 class MarkdownExplorer::Application < Sinatra::Base
   get "/" do
-    @files = Files.new(Dir.pwd)
+    @files = MarkdownExplorer::Files.new(Dir.pwd)
     erb :index
+  end
+
+  get "/show" do
+    markdown = File.read(params[:filename])
+    @markup = MarkdownExplorer::Renderer.new(markdown).markup
+    erb :show
   end
 end
